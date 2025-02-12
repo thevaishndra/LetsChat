c69d7a0de3f686bb76b5458626dd5dd62cff1cfb
thevaishndra <chandrav306m@gmail.com>
2025-02-12 22:22:28 +0530

    message routes - backend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
-import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.routes.js";
+
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
---
+++ backend/src/models/message.models.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    receierId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true,
+    },
+    text: {
+        type : String,
+    },
+    image: {
+        type : String,
+    },
+},{ timestamps : true});
