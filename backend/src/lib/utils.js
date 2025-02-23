526312eedfc467cf690fe78dad0c8b0dfb81617a
thevaishndra <chandrav306m@gmail.com>
2025-02-23 23:52:00 +0530

    cors issues

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "None", 
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
   });
 
   return token;
